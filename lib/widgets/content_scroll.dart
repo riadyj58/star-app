c4e3cc4fa86506c579cd7307648846fd78786f77
riadyj58 <riadyj58@gmail.com>
2019-12-17 15:50:03 +0700

    Content scroll sudah diedit

--- lib/widgets/content_scroll.dart
+++ lib/widgets/content_scroll.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:star_app/models/video_model.dart';
+import 'package:star_app/screens/movie_screen.dart';
 
 class ContentScroll extends StatelessWidget {
-  final List<String> images;
+  final List<Video> videos;
   final String title;
   final double imageHeight;
   final double imageWidth;
 
   ContentScroll({
-    this.images,
+    this.videos,
     this.title,
     this.imageHeight,
     this.imageWidth,
@@ -45,31 +47,89 @@ class ContentScroll extends StatelessWidget {
           child: ListView.builder(
             padding: EdgeInsets.symmetric(horizontal: 30.0),
             scrollDirection: Axis.horizontal,
-            itemCount: images.length,
+            itemCount: videos.length,
             itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                  vertical: 20.0,
-                ),
-                width: imageWidth,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black54,
-                      offset: Offset(0.0, 4.0),
-                      blurRadius: 6.0,
+              return Stack(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => MovieScreen(videos: videos[index]),
+                      ),
                     ),
-                  ],
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image(
-                    image: AssetImage(images[index]),
-                    fit: BoxFit.cover,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                        vertical: 20.0,
+                      ),
+                      width: imageWidth,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(0.0, 4.0),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          ClipRRect(
+                            borderRadius:
+                                BorderRadius.vertical(top: Radius.circular(10)),
+                            child: Image(
+                              image: AssetImage(videos[index].imageUrl),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    videos[index].title,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.remove_red_eye,
+                                        color: Colors.black,
+                                        size: 12,
+                                      ),
+                                      Text(
+                                        '100',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 12.0,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.vertical(
+                                      bottom: Radius.circular(10))),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                ],
               );
             },
           ),
